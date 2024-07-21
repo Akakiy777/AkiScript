092fc0208f6719ca05053b2d18259cae1b0707d2
Akakiy777 <136377706+Akakiy777@users.noreply.github.com>
2024-07-21 19:53:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 $('document').ready(function(){
-    function showFile(input) {
-        let file = input.files[0];
-      
-        alert(`File name: ${file.name}`); // например, my.png
-        alert(`Last modified: ${file.lastModified}`); // например, 1552830408824
-    }
-    showFile('folder.none')
+    alert(09);
 });
